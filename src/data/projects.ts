73f60cb1d0ac8cb3ec3dc752c37930fb9c48012d
Javi <130647819+JavieraAlmendrasVilla@users.noreply.github.com>
2025-06-22 11:25:25 +0200

    Fix boolean case error in projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -74,7 +74,7 @@ export const projects: Project[] = [
     technologies: ['R', 'ggplot2', 'data.table', 'tidyr', 'magrittr'],
     category: 'Data Analytics',
     codeUrl: 'https://github.com/JavieraAlmendrasVilla/Case-1-Oral-Health-data',
-    featured: False
+    featured: false
   },
   {
   id: '6',
@@ -88,6 +88,6 @@ export const projects: Project[] = [
   technologies: ['Python', 'Pandas', 'SQLite3', 'SQL'],
   category: 'Data Analytics',
   codeUrl: 'https://github.com/JavieraAlmendrasVilla/AntiFraudProject',
-  featured: False
+  featured: false
 }
 ];
